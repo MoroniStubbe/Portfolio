4a34dce03835e261f2318d46076745290d0ef59e
MoroniStubbe <moroni.stubbe@icloud.com>
2018-05-30 09:44:29 +0200

    commit

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 //fadeShow
 
-function fade(id, direction, duration, frames, to) {
+function fade(id, duration, frames, toOpacity) {
     var fps = frames / duration;
     var element = document.getElementById(id);
-    var opacity = element.style.opacity.value;
-    if (direction === "in") {
-        while(element.style.opacity.value < to){
-            setTimeout(1 / fps);
-        }
-    }else if(direction === "out"){
-        while(element.style.opacity.value > to){
-            element.style.opacity = opacity + (to - opacity) / fps;
-            setTimeout(1 / fps);
-        }
+    if(!element.style.opacity){element.style.opacity = "1"}
+    var step = (toOpacity - parseInt(element.style.opacity)) / fps;
+
+    while (element.style.opacity > toOpacity) {
+        element.style.opacity = parseInt(element.style.opacity) + step;
+        setTimeout(1 / fps);
     }
 }
+
 function fadeShow() {
-    fade('fadeImage', "out", 1, 30, 0);
-    setTimeout(5000);
-    fade('fadeImage', "in", 1, 30, 1);
+    var index = 1;
+    while(1 === 1){
+        fade('fadeImage', "out", 1, 30, 0);
+        setTimeout(5000);
+        document.getElementById('fadeImage').src = "images/index/fadeShow/" + (index + 1);
+        index++;
+    }
 }
